b22178e95bea1cd8714c6e015a82356a449c53a2
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-11-22 19:54:41 +0100

    Update bucket-script

--- scripts/setup_buckets.sh
+++ scripts/setup_buckets.sh
@@ -2,6 +2,7 @@ S3_BUCKET_SRC=trady-cloud-src
 S3_BUCKET_TERRAFORM=trady-cloud-terraform
 REGION=us-west-2
 
+# Create bucket for resources
 if aws s3 ls "s3://$S3_BUCKET_SRC" 2>&1 | grep -q 'An error occurred.'
 then
     aws s3api create-bucket --bucket $S3_BUCKET_SRC --region $REGION --create-bucket-configuration LocationConstraint=$REGION
@@ -9,6 +10,7 @@ else
     echo "Bucket '$S3_BUCKET_SRC' already exists."
 fi
 
+# Create bucket for Terraform
 if aws s3 ls "s3://$S3_BUCKET_TERRAFORM" 2>&1 | grep -q 'An error occurred.'
 then
     aws s3api create-bucket --bucket $S3_BUCKET_TERRAFORM --region $REGION --create-bucket-configuration LocationConstraint=$REGION
