eef258712f3e937ece68a1f9b90f851c44da8208
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-11-22 23:08:31 +0100

    Update prerequisites.sh to create main.tf and variables.tf

--- scripts/prerequisites.sh
+++ scripts/prerequisites.sh
@@ -4,3 +4,7 @@ REGION=us-west-2
 
 # Create buckets necessary for Terraform via script as the sandbox does not allow creating them via Terraform itself
 sh setup_buckets.sh $REGION $S3_BUCKET_SRC $S3_BUCKET_TERRAFORM 
+
+# Create from terraform-templates with variables: main.tf & variables.tf
+sed -e s/REGION/${REGION}/ -e s/S3_BUCKET_TERRAFORM/${S3_BUCKET_TERRAFORM}/  < ../infrastructure/templates/main.tf.template > ../infrastructure/main.tf
+sed s/REGION/${REGION}/ < ../infrastructure/templates/variables.tf.template > ../infrastructure/variables.tf
