70f435b8f74b6bdf2e6472c9720b9e44a3738b93
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-11-22 23:19:13 +0100

    Fix: typo setup_buckets.sh

--- scripts/setup_buckets.sh
+++ scripts/setup_buckets.sh
@@ -1,7 +1,8 @@
 # Create bucket for resources in chosen region
 if aws s3 ls "s3://$2" 2>&1 | grep -q 'An error occurred.'
 then
-    aws s3api create-bucket --bucket $S1 --region $1 --create-bucket-configuration LocationConstraint=$1
+    aws s3api create-bucket --bucket $2 --region $1 --create-bucket-configuration LocationConstraint=$1
+    echo "Bucket '$2' has been created."
 else
     echo "Bucket '$2' already exists."
 fi
@@ -10,6 +11,7 @@ fi
 if aws s3 ls "s3://$3" 2>&1 | grep -q 'An error occurred.'
 then
     aws s3api create-bucket --bucket $3 --region $1 --create-bucket-configuration LocationConstraint=$1
+    echo "Bucket '$3' has been created."
 else
     echo "Bucket '$3' already exists."
 fi
