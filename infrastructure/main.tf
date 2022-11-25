27c1ca32827f467945eddae470deb3ae9ec10cdc
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-11-25 03:12:05 +0100

    Refactor: creation of terraform files

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,17 +1,11 @@
 terraform {
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
-
-   backend "s3" {
-    bucket = "trady-cloud-terraform"
-    key    = "terraform.tfstate"
-    region = "us-west-2"
-  }
+   backend "s3" {}
 }
 
 provider "aws" {
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -2,3 +2,8 @@ variable "aws_region" {
     type = string
     default = "us-west-2"
 }
+
+variable "s3bucket_terraform" {
+    type = string
+    default = "trady-cloud-terraform"
+}
