a9e02ce5e8671c23294988c4a5f5c4719148efde
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-11-25 01:29:00 +0100

    Refactor: bucket-scripting

--- scripts/prerequisites.sh
+++ scripts/prerequisites.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+
+source functions.sh
+
 S3_BUCKET_SRC=trady-cloud-src
 S3_BUCKET_TERRAFORM=trady-cloud-terraform
 REGION=us-west-2
 
 # Create buckets necessary for Terraform via script as the sandbox does not allow creating them via Terraform itself
-sh setup_buckets.sh $REGION $S3_BUCKET_SRC $S3_BUCKET_TERRAFORM 
+create_s3bucket_multi $REGION $S3_BUCKET_SRC $S3_BUCKET_TERRAFORM
 
 # Create from terraform-templates with variables: main.tf & variables.tf
 sed -e s/REGION/${REGION}/ -e s/S3_BUCKET_TERRAFORM/${S3_BUCKET_TERRAFORM}/  < ../infrastructure/templates/main.tf.template > ../infrastructure/main.tf
