685e4df5d956e2459a67942e304c8a1924e6db8e
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-11-29 17:49:28 +0100

    Fix: space after region in github variables

--- scripts/functions_github.sh
+++ scripts/functions_github.sh
@@ -5,5 +5,5 @@ create_gh_variables() {
     echo 'AWS_ACCESS_KEY_ID=${{ secrets.AWS_ACCESS_KEY_ID }}
 AWS_SECRET_ACCESS_KEY=${{ secrets.AWS_SECRET_ACCESS_KEY }}
 AWS_SESSION_TOKEN=${{ secrets.AWS_SESSION_TOKEN }}
-AWS_REGION='${1}\ > ../.github/variables/variables.env
+AWS_REGION='${1} > ../.github/variables/variables.env
 }
