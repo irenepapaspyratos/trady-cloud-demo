95fe9e1777b65d21839eac84815ee5a82ddcca78
irenepapaspyratos <101578008+irenepapaspyratos@users.noreply.github.com>
2022-11-29 12:04:47 +0100

    Fix: execution prerequisites.sh

--- scripts/functions_aws.sh
+++ scripts/functions_aws.sh
@@ -18,6 +18,12 @@ create_aws_s3bucket_multi {
     local buckets=("$@")
     for b in "${buckets[@]}";
         do
-            $(create_aws_s3bucket $region $b | cat)
+            if 
+                aws s3 ls "s3://$2" 2>&1 | grep -q 'An error occurred.'
+            then
+                aws s3api create-bucket --bucket $2 --region $1 --create-bucket-configuration LocationConstraint=$1 | cat
+            else
+                echo "Bucket '$2' already exists."
+            fi
         done
 }
