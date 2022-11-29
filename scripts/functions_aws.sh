b928f5919dc2c9cbf5b44c651521e2f80b1435c0
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-11-29 11:48:51 +0100

    Fix: execution prerequisites.sh

--- scripts/functions_aws.sh
+++ scripts/functions_aws.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create a single bucket in AWS S3
-create_aws_s3bucket() {
+create_aws_s3bucket {
     if 
         aws s3 ls "s3://$2" 2>&1 | grep -q 'An error occurred.'
     then
@@ -12,7 +12,7 @@ create_aws_s3bucket() {
 }
 
 # Create many buckets in AWS S3 in the same region
-create_aws_s3bucket_multi() {
+create_aws_s3bucket_multi {
     local region=$1
     shift
     local buckets=("$@")
