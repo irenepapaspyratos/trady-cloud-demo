e9cee9cf38ef4a2da0611ebce0664220bee037bb
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-11-30 12:18:32 +0100

    Update github-functions for variables

--- scripts/functions_github.sh
+++ scripts/functions_github.sh
@@ -2,8 +2,5 @@
 
 # Create variable file from template
 create_gh_variables() {
-    echo 'AWS_ACCESS_KEY_ID=${{ secrets.AWS_ACCESS_KEY_ID }}
-AWS_SECRET_ACCESS_KEY=${{ secrets.AWS_SECRET_ACCESS_KEY }}
-AWS_SESSION_TOKEN=${{ secrets.AWS_SESSION_TOKEN }}
-AWS_REGION='${1} > ../.github/variables/variables.env
+    echo 'AWS_REGION='${1} > ../.github/variables/variables.env
 }
