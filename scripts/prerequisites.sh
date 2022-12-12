81563edccd9c902b91f0ada6202e78e8b8147b9d
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-12-12 04:26:47 +0100

    Fix: prerequisites

--- scripts/prerequisites.sh
+++ scripts/prerequisites.sh
@@ -3,7 +3,6 @@
 . ./functions_aws.sh
 . ./functions_terraform.sh
 . ./functions_github.sh
-. ./functions_crawl.sh
 
 S3_BUCKET_SRC=trady-cloud-src
 S3_BUCKET_TERRAFORM=trady-cloud-terraform
@@ -36,7 +35,7 @@ create_tf_backend_s3 $REGION $S3_BUCKET_TERRAFORM
 #create_aws_lambda_layer crawl
 
 # Create from data-crawler-templates: variables.py
-create_crawl_variables "'$SYMBOL_BUCKETS'"
+#create_crawl_variables "'$SYMBOL_BUCKETS'"
 
 # Fill build-directory
 mv ../infrastructure/lambda-layer/**/*.zip build
