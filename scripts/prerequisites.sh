17dd0d99c47d1e51515c31dc470b80b6d6372a40
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-12-12 04:52:04 +0100

    Fix: prerequisites

--- scripts/prerequisites.sh
+++ scripts/prerequisites.sh
@@ -33,13 +33,13 @@ create_tf_variables $REGION $S3_BUCKET_TERRAFORM
 create_tf_backend_s3 $REGION $S3_BUCKET_TERRAFORM
 
 # Create Lambda-Layers
-create_aws_lambda_layer crawl
+#create_aws_lambda_layer crawl
 
 # Create from data-crawler-templates: variables.py
 create_crawl_variables "$SYMBOL_BUCKETS"
 
 # Fill build-directory
-mv ../infrastructure/lambda-layer/**/*.zip build
+#mv ../infrastructure/lambda-layer/**/*.zip build
 
 # Upload build-directory to src-bucket
 aws s3 cp build s3://$S3_BUCKET_SRC/ --recursive
