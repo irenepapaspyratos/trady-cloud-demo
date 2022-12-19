7a652105c757329147fb4d24a4938366e0e5d771
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-12-19 13:10:12 +0100

    Test again

--- scripts/ranges.sh
+++ scripts/ranges.sh
@@ -12,7 +12,7 @@ create_ranges() {
             value="${symbol##*:}"
             symbol_stamp=$(date -j -f "%Y%m%d%H" "${value}" "+%s")
             hours=$((($TODAY_STAMP - $symbol_stamp)/(60*60)))
-            RANGE="$RANGE,\"$key\"=$hours"
+            RANGE="$RANGE,\"$key\":$hours"
         done
 
     echo "$RANGE" | sed -r 's/^,//'
